fb01d932abbe8c27a3bbcb2c5e409ee8a8b526b0
AdrianaEG <adriana.ngc438@gmail.com>
2020-07-27 17:34:41 -0300

    Arreglo funcionalidad filtros desplegables

---
+++ site/public/script/shop.js
@@ -0,0 +1,41 @@
+let $desplegables = document.querySelectorAll('.menu-desplegable');
+let $btnCerrar = document.querySelector('.cerrar');
+
+/*Desplegar filtros*/
+document.querySelector('.filtrar').onclick = function(e){
+    let $filtros = document.querySelector('.filtros');
+    if($filtros.classList.contains('oculto')){
+        $filtros.classList.remove('oculto');
+        $filtros.style.display = "block";
+    }
+    else{
+        $filtros.classList.add('oculto');
+        $filtros.style.display="none";
+    }
+    e.preventDefault();
+}
+
+/*Cerrar filtros al hacer click en cerrar*/
+$btnCerrar.addEventListener('click', cerrarFiltros);
+function cerrarFiltros(){
+    document.querySelector('.filtros').classList.add('oculto');
+    document.querySelector('.filtros').style.display = "none";
+}
+
+/*Desplegar cada uno de los filtros (sexo, talle y deportes)*/
+$desplegables.forEach($desplegable=>{
+    $desplegable.onclick = function(e){
+        let $elementoDondeHizoClick = e.target;
+        let $contenedorContOculto = $elementoDondeHizoClick.parentNode.parentNode;
+        let $contenidoOculto = $contenedorContOculto.querySelector('.contenido');
+        
+        if($contenidoOculto.classList.contains('oculto')){
+            $contenidoOculto.classList.remove('oculto');
+        }
+        else{
+            $contenidoOculto.classList.add('oculto');
+        }
+        e.preventDefault();
+    }
+});
+
