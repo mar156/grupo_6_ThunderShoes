72f505ff8fd01872c0ddc9770d3a28ec7d8e57bf
MatuteDV <matiasvieira0@gmail.com>
2020-08-08 23:06:43 -0300

    modificaciones pequeñas de espaciado en public/script/script.js

--- site/public/script/script.js
+++ site/public/script/script.js
@@ -3,26 +3,13 @@ let menu = document.getElementById("menu");
 
 let summaryCart = document.getElementById("summary-container");
 
-/* 
-let thumnails = document.getElementsByClassName("thumnails"); */
-
-
 btn.addEventListener("click", function(e){
-
-if(menu.style.left == "-100vw"){
-
-
-    menu.style.left = "0vw";
-
-} else {
-
-
-    menu.style.left = "-100vw";
-
-}
-
+    if(menu.style.left == "-100vw"){
+        menu.style.left = "0vw";
+    } else {
+        menu.style.left = "-100vw";
+    }
 });
-
 
 function toogleSummary () {
     if ( summaryCart.style.display === "none" ) {
@@ -32,24 +19,17 @@ function toogleSummary () {
     }
 }
 
-
 /* Prueba slideshow product detail*/
 
 let thumnails = document.getElementsByClassName("thumnails");
-
 let selectedImg = document.getElementsByClassName("thumnails selected")
 
 for(let i = 0; i < thumnails.length; i++){
-
     thumnails[i].addEventListener("mouseover", function(){
-       
         if(selectedImg.length > 0){
             selectedImg[0].classList.remove("selected");
         }
- 
         this.classList.add("selected");
         document.getElementById("img-principal").src = this.src;
-
-    })
-
+    });
 }
