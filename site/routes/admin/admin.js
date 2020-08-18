540eb36ced28dd47b842f3a171e491695df85b43
MatuteDV <vieiramatias@hotmail.com>
2020-08-18 15:07:56 -0300

    Definidas las 7 rutas BREAD

--- site/routes/admin/admin.js
+++ site/routes/admin/admin.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const path = require('path');
 const adminController= require(path.join(__dirname, '/../../controllers/adminController'));
 
-router.get('/product/create', adminController.createProduct);
-router.get('/product/edit/:id', adminController.editProduct);
+// router.get('/product', adminController.listProduct);             // Vista    - Listado de productos
+// router.get('/product/:id', adminController.detailProduct);       // Vista    - Detalle de producto
+
+router.get('/product/create', adminController.createProduct);       // Vista    - Creación de productos
+// router.post('/product/', adminController.addProduct);            // Post     - Agregar un producto
+
+router.get('/product/:id/edit', adminController.editProduct);       // Vista    - Editar producto
+// router.put('/product/:id', adminController.updateProduct);       // Put      - Editar producto
+
+// router.delete('product/:id', adminController.deleteProduct);     // Delete   - Eliminar producto
 
 module.exports = router;
