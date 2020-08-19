fd7346799ac44d31d540b921504002532d9a3406
AdrianaEG <adriana.ngc438@gmail.com>
2020-08-19 01:52:09 -0300

    Carga el shop con los productos que hay en JSON

--- site/controllers/productController.js
+++ site/controllers/productController.js
@@ -1,8 +1,16 @@
 const path = require ('path')
-
+const fs = require('fs');
+const filePath =  path.join(__dirname, '../data/productsExample.json');
 const controller = {
     index: (req, res) => {
-        res.render('products/shop');
+        //Leer el json con los productos
+        //Traer los productos
+        //Mandarselos a la vista en formato Javascript
+        //Recibirlos en la vista como corresponde... 
+        let products = fs.readFileSync(filePath, 'utf-8');
+        products = JSON.parse(products);
+        //res.send(products);
+        res.render('products/shop', {products});
     },
     detail: (req, res)=>{
         res.render('products/productDetail');
