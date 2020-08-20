ab37fe566b5896ec35baeeeedfa54526d09f705d
MatuteDV <vieiramatias@hotmail.com>
2020-08-20 11:02:43 -0300

    se habilitan las rutas ya desarrolladas

--- site/routes/admin/admin.js
+++ site/routes/admin/admin.js
@@ -4,15 +4,15 @@ const path = require('path');
 const adminController= require(path.join(__dirname, '/../../controllers/adminController'));
 
 // http://localhost:3000/admin/product/
-router.get('/product', adminController.listProduct);             // Vista    - Listado de productos 
+router.get('/product', adminController.listProduct);                // Vista    - Listado de productos 
 
 
-//router.get('/product/create', adminController.createProduct);       // Vista    - Creación de productos
-//router.post('/product/', adminController.addProduct);            // Post     - Agregar un producto
+router.get('/product/create', adminController.createProduct);     // Vista    - Creación de productos
+//router.post('/product/', adminController.addProduct);             // Post     - Agregar un producto
 
-//router.get('/product/:id/edit', adminController.editProduct);       // Vista    - Editar producto
-//router.put('/product/:id', adminController.updateProduct);       // Put      - Editar producto
+router.get('/product/:id/edit', adminController.editProduct);     // Vista    - Editar producto
+//router.put('/product/:id', adminController.updateProduct);        // Put      - Editar producto
 
-//router.delete('product/:id', adminController.deleteProduct);     // Delete   - Eliminar producto
+//router.delete('product/:id', adminController.deleteProduct);      // Delete   - Eliminar producto
 
 module.exports = router;
