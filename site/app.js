d2cb19c606a3c5727edff929b2ddda1074c92969
MatuteDV <vieiramatias@hotmail.com>
2020-08-20 11:35:01 -0300

    Se agrega app.use() de methodOverride al entry point

--- site/app.js
+++ site/app.js
@@ -4,8 +4,10 @@ const productRoutes = require('./routes/product/product');
 const mainRoutes = require('./routes/statics/main');
 const usersRoutes = require('./routes/users/users');
 const adminRoutes = require('./routes/admin/admin');
+const methodOverride = require('method-override');
 
 app.set( 'view engine', 'ejs');
+app.use(methodOverride('_method'));
 
 app.use(express.static('public'));
 app.use('/product', productRoutes);
