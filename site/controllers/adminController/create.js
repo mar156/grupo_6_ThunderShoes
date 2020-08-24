831afb04ee39fb2908a5ee9e5d217bf3fb83bb0b
mar156 <m.n.v_156@hotmail.com.ar>
2020-08-24 14:59:50 -0300

    Agregué el comando de id e hice la prueba

--- site/controllers/adminController/create.js
+++ site/controllers/adminController/create.js
@@ -1,16 +1,26 @@
 const path = require ('path')
 const fs = require('fs');
 const filePath =  path.join(__dirname, '../../data/products.json');
+  
+
 
 const createProducts = {
     //createVista: function (req, res) {
     //res.render()
     //},
     addProduct: function(req,res) {
+
         let productsJSON = fs.readFileSync(filePath, 'utf-8');
         let products= JSON.parse(productsJSON);
+        let indiceMayor = 0;
+            products.forEach( producto => {
+            indiceMayor = producto.id > indiceMayor ? producto.id : indiceMayor;
+        })
+        let siguienteIndice = indiceMayor + 1;
+
+
         let newProduct = {
-            id: 1,
+            id: siguienteIndice,
             name: req.body.name,
             brand: req.body.brand,
             description: req.body.description,
@@ -20,8 +30,8 @@ const createProducts = {
             colors: req.body.colors,
             sizes: req.body.sizes,
             price: req.body.price,
-            image: "" ,
-            stock: "", //cuando tengamos una planilla de stock
+            image: " " ,
+            stock: 1, //cuando tengamos una planilla de stock
             //puedo usarla para leer la cantidad de productos de ese tipo que
             //hay y actualizar el número en función a ello
             
