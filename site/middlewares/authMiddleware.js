974bb709d04c0af37ee3c917a4aa29e810397bc6
stefanovallarella <stefanovallarella@gmail.com>
2020-09-01 12:08:37 -0300

    Correccion authMiddleware

--- site/middlewares/authMiddleware.js
+++ site/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ function authMiddleware(req, res, next){
     if(req.session.userLoggedIn != undefined){
         next();
     }else{
-        res.redirect("users/login");
+        res.redirect("/users/login");
     }
 }
 
