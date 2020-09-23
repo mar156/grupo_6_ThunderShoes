f08f5d5f0500ab7fe132dd6bc58c1dd018f1f99e
AdrianaEG <adriana.ngc438@gmail.com>
2020-09-23 20:21:21 -0300

    Agrego detalle de producto sequelize

--- site/controllers/productController/detail.js
+++ site/controllers/productController/detail.js
@@ -1,15 +1,23 @@
 const path = require ('path')
 const fs = require('fs');
 const filePath =  path.join(__dirname, '../../data/products.json');
+const db = require('../../database/models');
 
 module.exports = (req, res) => { 
     let id = Number(req.params.id);
-    let products = fs.readFileSync(filePath, 'utf-8');
+    db.Product.findByPk(id)
+        .then((product)=>{
+            res.render('products/productDetail', {product});
+        })
+        .catch(error=>{
+            res.status(404).send('Página no encontrada - Vista error 404 pendiente de construir')
+        });
+    /*let products = fs.readFileSync(filePath, 'utf-8');
     products = JSON.parse(products);
     let product = products.find(product => product.id === id);
     if (product) {
         res.render('products/productDetail', {product});
     } else {
         res.status(404).send('Página no encontrada - Vista error 404 pendiente de construir');
-    }
+    }*/
 };
