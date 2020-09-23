eb7b516dbf42e5c1119cdf549c4616bc988cc437
stefanovallarella <stefanovallarella@gmail.com>
2020-09-23 14:22:57 -0300

    Edit/Mostrar producto a Editar en la vista  edición de productos funcionando con Base de datos SQL/Sequelize

--- site/controllers/adminController/edit.js
+++ site/controllers/adminController/edit.js
@@ -2,17 +2,134 @@ const path = require ('path')
 const fs = require('fs');
 const filePath =  path.join(__dirname, '../../data/products.json');
 
+const {product, brand, gender, image, category, user, color, size } = require('../../database/models');
 
 const editProduct = {
     edit: function(req, res){
+        
         let id = req.params.id;
-        let readProducts = fs.readFileSync(filePath, 'utf-8');
+        /* let readProducts = fs.readFileSync(filePath, 'utf-8');
         let products = JSON.parse(readProducts);
         let productToEdit = products.find(function(product){
             return product.id == id;
-        });
+        }); */
+        
+        product.findByPk(id, { include: [brand, gender, image, category, color, size] })
+       .then(function(productToEdit){
+        
+            let productToShow = {
+                name: productToEdit.name,
+                brand: {
+                    newbalance:'',
+                    puma: '',
+                    nike:'',
+                    adidas:''
+                },
+                description: productToEdit.description,
+                gender: {
+                    male:'',
+                    female: ''
+                },
+                category: {
+                    tennis: '',
+                    running: '',
+                    volley: '',
+                    football: '',
+                    collection: ''
+                },
+                onSale: productToEdit.onSale,
+                colors: {                  // la lista de colors se carga desde BD/FileJson y se genera el listado de la propiedad colors, 
+                    red: '',                // de cada producto se obtiene que colors seleccionados posee y se marca en 'checked' para pasar a la vista. 
+                    green: '',              // Idem 'Talles' y 'Categorías'.
+                    black: '',
+                    blue: '',
+                    yellow: '',
+                    gray: '',
+                    white: ''
+                },
+                sizes: {
+                    t34: '',
+                    t35: '',
+                    t36: '',
+                    t37: '',
+                    t38: '',
+                    t39: '',
+                    t40: ''
+                },
+                price: productToEdit.price,
+                image: [
+                    'file1.jpg',
+                    'file2.jpg'
+                ],
+                stock: productToEdit.stock
+            };
 
-        let product = {
+            // Marca
+        
+            for (const propiedad in productToShow.brand) {
+                if (productToEdit.brand.name.split(" ").join("").toLowerCase() == propiedad) {
+                    productToShow.brand[propiedad] = 'checked';
+                } 
+            }
+
+            // Género
+
+            for (const propiedad in productToShow.gender) {
+                if (productToEdit.gender.name.toLowerCase() == propiedad) {
+                    productToShow.gender[propiedad] = 'checked';
+                } 
+            }
+
+            // Categorias
+
+            productToEdit.categories.forEach(function(category){
+                for (const propiedad in productToShow.category) {
+                    if (category.name.toLowerCase() == propiedad) {
+                        productToShow.category[propiedad] = 'checked';
+                    } 
+                }
+            });  
+
+            // Colores
+
+            productToEdit.colors.forEach(function(color){
+                for (const propiedad in productToShow.colors) {
+                    if (color.code == "0011" && propiedad == "red") {
+                        productToShow.colors[propiedad] = 'checked';
+                    } 
+                    else if (color.code == "0022"&& propiedad == "green"){ productToShow.colors[propiedad] = 'checked';}
+                    else if (color.code == "0033" && propiedad == "black"){ productToShow.colors[propiedad] = 'checked';}
+                    else if (color.code == "0044" && propiedad == "blue"){ productToShow.colors[propiedad] = 'checked';}
+                    else if (color.code == "0055" && propiedad == "yellow"){ productToShow.colors[propiedad] = 'checked';}
+                    else if (color.code == "0066" && propiedad == "grey"){ productToShow.colors[propiedad] = 'checked';}
+                    else if (color.code == "0077" && propiedad == "white"){ productToShow.colors[propiedad] = 'checked';}
+                }
+            });
+
+            // Talles
+
+            productToEdit.sizes.forEach(function(size){
+                for (const propiedad in productToShow.sizes) {
+                    if (("t"+size.size) == propiedad) {
+                        productToShow.sizes[propiedad] = 'checked';
+                    } 
+                }
+            }); 
+
+
+        res.render('admin/editProduct', { product: productToShow , id });
+
+        })
+        .catch(function(error){
+             console.log(error);
+        });   
+         
+
+        //    res.render('admin/editProduct', { product , id }); 
+           /* res.send(producto); */    
+      
+        
+       /*  let product = {
             name: productToEdit.name,
             brand: {
                 newbalance:'',
@@ -117,9 +234,9 @@ const editProduct = {
             } 
         }
          
-      
-
-        res.render('admin/editProduct', { product , id });
+       */
+        
+        /* res.render('admin/editProduct', { product , id }); */
 
     },
     update: function(req, res){
