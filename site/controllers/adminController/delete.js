d3863314ff778b16a0d24481b68e9c7716d1499f
stefanovallarella <stefanovallarella@gmail.com>
2020-09-24 17:28:15 -0300

    Destroy/Eliminar productos para admin funcionando

--- site/controllers/adminController/delete.js
+++ site/controllers/adminController/delete.js
@@ -2,12 +2,22 @@ const path = require ('path')
 const fs = require('fs');
 const filePath =  path.join(__dirname, '../../data/products.json');
 
-module.exports = (req, res) => {
-    let id = Number(req.params.id);
+const {product, brand, gender, image, category, user, color, size } = require('../../database/models');
+
+module.exports = async (req, res) => {
+    /* let id = Number(req.params.id);
     let products = fs.readFileSync(filePath, 'utf-8');
     products = JSON.parse(products);
     products = products.filter( product => product.id !== id )
     fs.writeFileSync(filePath, JSON.stringify(products), 'utf-8');
+ */
+    let id = req.params.id;
+    productExist = await product.findByPk(id);
 
-    res.redirect('/admin/product/');
+    try{
+    await product.destroy({ where:{id: id}})
+    return res.redirect('/admin/product/');
+    }catch(error){
+        res.send("Error en borrar el producto con ID: " + id);
+    }
 }
