95356fa58b4c1257ba2bac7ab28d9439ce0ff291
MatuteDV <matiasvieira0@gmail.com>
2020-09-23 23:16:44 -0300

    Se agrega a migrate la FK

--- site/database/migrations/20200916233134-create-users.js
+++ site/database/migrations/20200916233134-create-users.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      category_id: {
+        type: Sequelize.INTEGER
+      },
       first_name: {
         type: Sequelize.STRING
       },
