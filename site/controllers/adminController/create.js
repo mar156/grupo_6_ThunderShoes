3b3cf7155163bf9179e24690239cfdb9b8c6afec
stefanovallarella <stefanovallarella@gmail.com>
2020-09-24 16:30:07 -0300

    Avances Creado de producto en Base de datos con sequelize (set / add)

--- site/controllers/adminController/create.js
+++ site/controllers/adminController/create.js
@@ -2,23 +2,76 @@ const path = require ('path')
 const fs = require('fs');
 const filePath =  path.join(__dirname, '../../data/products.json');
   
-
+const {product, brand, gender, image, category, user, color, size } = require('../../database/models');
 
 const createProducts = {
     //createVista: function (req, res) {
     //res.render()
     //},
-    addProduct: function(req,res) {
+    addProduct: async function(req,res) {
 
-        let productsJSON = fs.readFileSync(filePath, 'utf-8');
+     /*    let productsJSON = fs.readFileSync(filePath, 'utf-8');
         let products= JSON.parse(productsJSON);
         let indiceMayor = 0;
             products.forEach( producto => {
             indiceMayor = producto.id > indiceMayor ? producto.id : indiceMayor;
         })
-        let siguienteIndice = indiceMayor + 1;
+        let siguienteIndice = indiceMayor + 1; */
+
+        let newProduct = req.body;
+
+        // console.log(newProduct);
+
+        try {
+
+            let images = req.image;
+            /* let categories = req.body.category; */
+            let categories = [1,3,5];
+            /* let colors = req.body.color; */
+            let colors = [1, 2];
+            /* let sizes = req.body.sizes; */
+            let sizes = [1, 2];
+            let genders = req.body.gender;
+            let brands = req.body.brand;
+           
+            
+            let newProduct = await product.create({
+                name: req.body.name,
+                price: req.body.price,
+                on_sale: req.body.on_sale,
+                description: req.body.description,
+                brand_id: Number(2),
+                gender_id: Number(1)
+                
+            }, {
+                include: [brand, gender, image, category, color, size ]
+            });
+
+            /* newProduct.setImages(images); */
+            newProduct.setCategories(categories);
+            newProduct.setColors(colors);
+            newProduct.setSizes(sizes);
 
 
+           /*  newProduct.addImages(images);
+            newProduct.addCategories(categories);
+            newProduct.addColors(colors);
+            newProduct.addSizes(sizes); */
+            
+
+            console.log(JSON.stringify(newProduct));
+            console.log(newProduct);
+
+            res.redirect('/admin/product/');
+        }catch (error) {
+            console.log(error);
+            res.status(500).send('Error');
+        }
+        }
+
+
+      
+        /* 
         let newProduct = {
             id: siguienteIndice,
             name: req.body.name,
@@ -42,8 +95,8 @@ const createProducts = {
         products.push(newProduct);
         let listaProductosJSON= JSON.stringify(products, null, " ");
         fs.writeFileSync(filePath, listaProductosJSON, 'utf-8');
-        return res.send(newProduct);
-}
+        return res.send(newProduct); */
+
 
 };
 
