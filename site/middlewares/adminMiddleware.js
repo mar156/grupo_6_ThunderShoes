a7eec8af727672deb33ac6acd763e1e44ebf6798
MatuteDV <matiasvieira0@gmail.com>
2020-09-23 23:30:58 -0300

    Se actualiza adminMiddleware

--- site/middlewares/adminMiddleware.js
+++ site/middlewares/adminMiddleware.js
@@ -1,5 +1,5 @@
 function adminMiddleware(req, res, next){
-    if(req.session.userLoggedIn != undefined && req.session.userLoggedIn.category == "admin" ){
+    if(req.session.userLoggedIn != undefined && req.session.userLoggedIn.category_user.name == "admin" ){
         next();
     }else{
         res.redirect("/");
