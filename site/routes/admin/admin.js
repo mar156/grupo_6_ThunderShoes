228b568adcec0e39e0a5de8346aaf08c42a7dd71
AdrianaEG <adriana.ngc438@gmail.com>
2020-09-29 10:10:50 -0300

    No funciona - subida de imagenes al crear producto

--- site/routes/admin/admin.js
+++ site/routes/admin/admin.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const adminController= require(path.join(__dirname, '/../../controllers/adminController'));
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: (req, file, callback)=>{
+        callback(null, path.join(__dirname,'../../public/img'))//carpeta donde se guardará el archivo
+    },
+    filename: (req, file, callback)=>{
+        callback(null, 'zapatilla' + Date.now() + path.extname(file.originalname))//nombre con el que se guardará el archivo
+    }
+});
+var upload = multer({storage:storage});
 
 
 // http://localhost:3000/admin/product/
@@ -9,7 +20,7 @@ router.get('/', adminController.listProduct)                        // Vista    
 router.get('/product', adminController.listProduct);                // Vista    - Listado de productos 
 
 router.get('/product/create', adminController.createProduct);       // Vista    - Creación de productos
-router.post('/product/create', adminController.addProduct);         // Post     - Agregar un producto
+router.post('/product/create', upload.array('image', 4),adminController.addProduct);         // Post     - Agregar un producto
 
 router.get('/product/:id/edit', adminController.editProduct);       // Vista    - Editar producto
 router.put('/product/:id', adminController.update);                 // Put      - Editar producto
