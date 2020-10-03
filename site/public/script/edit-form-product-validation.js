be43629b9322e6c390127b417ceed503df911674
stefanovallarella <stefanovallarella@gmail.com>
2020-10-03 20:27:45 -0300

    Correcciones Script validación front Edicion de productos

--- site/public/script/edit-form-product-validation.js
+++ site/public/script/edit-form-product-validation.js
@@ -1,101 +1,91 @@
 
 let form = document.getElementById('product');
 
-form.addEventListener("submit", function(e){   
+    const genderElements = document.getElementsByName('gender');
+    const brandElements = document.getElementsByName('brand');
+    const categoryElements = document.getElementsByName('category');
+    const colorElements = document.getElementsByName('colors');
+    const sizeElements = document.getElementsByName('sizes');
     
-    let genders = document.getElementsByName("gender");
-    let brands = document.getElementsByName("brand");
-    let categories = document.getElementsByName("category");
-    let colors = document.getElementsByName("colors");
-    let sizes = document.getElementsByName("sizes");
     
-    // Géneros    
-    let genderError = true;
-    genders.forEach(function(gender){
-        if(!gender.checked){
-            genderError = false;
-        }
-    }) 
-        if(!genderError){
-            document.getElementById('gender_error').innerText = '';
-        } else{
+    form.addEventListener('submit', function (e) {
+        let hasErrors = { 
+            gender: true,
+            brand: true,
+            category: true,
+            color: true,
+            size: true
+        };
+
+        // Hay errores hasta que se demuestre lo contrario. 
+        genderElements.forEach(function(gender){
+            if(gender.checked){
+                hasErrors.gender = false; 
+            }else{
+                hasErrors.gender = hasErrors.gender;
+            }
+        })
+
+        brandElements.forEach(function(brand){
+            if(brand.checked){
+                hasErrors.brand = false;
+            }else{
+                hasErrors.brand = hasErrors.brand;
+            }
+        })
+
+        categoryElements.forEach(function(category){
+            if(category.checked){
+                hasErrors.category = false;
+            }else{
+                hasErrors.category = hasErrors.category;
+            }
+        })
+
+        colorElements.forEach(function(color){
+            if(color.checked){
+                hasErrors.color = false;
+            }else{
+                hasErrors.color = hasErrors.color;
+            }
+        })
+
+        sizeElements.forEach(function(size){
+            if(size.checked){
+                hasErrors.size = false;
+            }else{
+                hasErrors.size = hasErrors.size;
+            }
+        })
+
+        if ( hasErrors.gender || hasErrors.brand || hasErrors.category || hasErrors.color || hasErrors.size || 
+            hasErrors.name || hasErrors.price || hasErrors.onSale || hasErrors.description || hasErrors.image 
+        ){ 
             e.preventDefault();
-            document.getElementById('gender_error').innerText = 'Debe seleccionar un género';
+            document.getElementById('gender_error').innerText = hasErrors.gender ? 'Debe seleccionar un género': '';
+            document.getElementById('brand_error').innerText = hasErrors.brand ? 'Debe seleccionar una marca': '';
+            document.getElementById('category_error').innerText = hasErrors.category ? 'Debe seleccionar al menos una categoría' : '';
+            document.getElementById('color_error').innerText = hasErrors.color ? 'Debe seleccionar al menos un color' : '';
+            document.getElementById('size_error').innerText = hasErrors.size ? 'Debe seleccionar al menos un talle' : '';
         }
-
-
-    // Marcas
-    let brandError = true;
-    brands.forEach(function(brand){
-        if(!brand.checked){
-            brandError = false;
-        }
-    }) 
-            if(!brandError){
-                document.getElementById('brand_error').innerText = '';
-            } else{
-                e.preventDefault();
-                document.getElementById('brand_error').innerText = 'Debe seleccionar una marca';
-            }
-
-    // Categorías
-    let categoryError = true;
-    categories.forEach(function(category){
-        if(!category.checked){
-            categoryError = false;
-        }
-    }) 
-            if(!categoryError){
-                document.getElementById('category_error').innerText = '';
-            } else{
-                e.preventDefault();
-                document.getElementById('category_error').innerText = 'Debe seleccionar al menos una categoría';
-            }
-
-
-    // Colores
-
-    let colorError = true;
-    colors.forEach(function(color){
-        if(!color.checked){
-            colorError = false;
-        }
-    }) 
-            if(!colorError){
-                document.getElementById('color_error').innerText = '';
-            } else{
-                e.preventDefault();
-                document.getElementById('color_error').innerText = 'Debe seleccionar al menos un color';
-            }
-
-    // Talles
-
-    let sizeError = true;
-    sizes.forEach(function(size){
-        if(!size.checked){
-            sizeError = false;
-        }
-    }) 
-            if(!sizeError){
-                document.getElementById('size_error').innerText = '';
-            } else{
-                e.preventDefault();
-                document.getElementById('size_error').innerText = 'Debe seleccionar al menos un talle';
-            }
-
-})
-
-
+    
+    });
+    
 
 // Nombre
 
 let nameInput = document.getElementById("name");
 nameInput.addEventListener("blur", function(){
     let nameError = document.getElementById("name_error");
+    if(!this.value){
+        nameError.innerText = "El nombre no puede estar vacío";
+        this.classList.add("error-input");
+    }
     if(this.value && this.value.length < 5){
         nameError.innerText = "El nombre debe tener al menos 5 caracteres";
         this.classList.add("error-input");
-    }else if (this.value.length >= 5){
+    }
+    if(this.value.length >= 5){
         this.classList.remove("error-input");
         nameError.innerText = "";
     }   
@@ -177,8 +167,5 @@ inputImage.addEventListener("change", function () {
     this.classList.remove("error-input");
     }
 });
-
-
-
 
 
