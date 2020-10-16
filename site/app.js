0ee3644d3786ce52c8e912363380aad9304d7c9f
MatuteDV <matiasvieira0@gmail.com>
2020-10-16 09:24:10 -0300

    Se agrega ruta /api al entry point

--- site/app.js
+++ site/app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+const session = require("express-session");
+const cookieParser = require('cookie-parser');
+
+const adminMiddleware = require('./middlewares/adminMiddleware');
+const rememberMiddleware = require('./middlewares/rememberMiddleware');
+const userMiddleware = require('./middlewares/userMiddleware');
+
 const productRoutes = require('./routes/product/product');
 const mainRoutes = require('./routes/statics/main');
 const usersRoutes = require('./routes/users/users');
 const adminRoutes = require('./routes/admin/admin');
-const methodOverride = require('method-override');
-const session = require("express-session");
-const bcrypt = require('bcryptjs');
-const cookieParser = require('cookie-parser');
-const adminMiddleware = require('./middlewares/adminMiddleware');
-const rememberMiddleware = require('./middlewares/rememberMiddleware');
-const userMiddleware = require('./middlewares/userMiddleware');
+const apiRoutes = require('./routes/api/api');
 
 app.set( 'view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(session({secret: "Clave Secreta", resave: true, saveUninitialized: true, }));
-// app.use(session({secret: "Clave Secreta"}));
 app.use(cookieParser());
 app.use(rememberMiddleware);
 app.use(userMiddleware);
@@ -27,6 +28,7 @@ app.use('/product', productRoutes);
 app.use('/', mainRoutes);
 app.use('/users', usersRoutes);
 app.use('/admin', adminMiddleware , adminRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(3000, ()=>{
     console.log('Servidor escuchando en el puerto 3000');
