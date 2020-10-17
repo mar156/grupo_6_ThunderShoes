33c49c52cff990f3bfd68000e7ba17e93520bf3b
MatuteDV <matiasvieira0@gmail.com>
2020-10-17 11:08:56 -0300

    Endpoint products/latest listo para verificar

--- site/controllers/api/productController/latest.js
+++ site/controllers/api/productController/latest.js
@@ -1,11 +1,54 @@
+const {product, brand, gender, image, category, color, size, sequelize } = require('../../../database/models');
+
 module.exports = (req, res) => {
     let response = {
         meta: {
-            status: 'ok'
+            status: 500,
+            msg: 'ocurrió un error inesperado',
         },
         data: {
-
+            product: {
+                id: -1,
+                name: '',
+                brand: '',
+                description: '',
+                gender: '',
+                discount: -1,
+                price: -1,
+                images: [],
+                categories: [],
+                colors: [],
+                sizes: []
+            },
         }
     }
-    res.json(response)
+
+    product.findOne({ 
+        order: [
+            ['id', 'DESC']
+        ],
+        include: [ brand, gender, image, category, color, size ]
+    })
+    .then( result => JSON.parse(JSON.stringify(result)))
+    .then( result => {
+        response.meta.status = 200;
+        response.meta.msg = 'última zapatilla encontrada exitosamente';
+
+        response.data.product.id = result.id;
+        response.data.product.name = result.name;
+        response.data.product.description = result.description;
+        response.data.product.price = result.price;
+        response.data.product.discount = result.on_sale;
+        response.data.product.brand = result.brand.name;
+        response.data.product.gender = result.gender.name;
+        response.data.product.images = result.images.map( image => `/img/${image.file_name}` );
+        response.data.product.categories = result.categories.map( category => category.name );
+        response.data.product.colors = result.colors.map( color => color.code );
+        response.data.product.sizes = result.sizes.map( size => size.size );
+        res.json(response)
+    })
+    .catch( err => {
+        console.log(err);
+        res.status(500).json(response)
+    });
 }
