3e4cea6992eb07bcc9a98506a18155a48d62a1bc
MatuteDV <matiasvieira0@gmail.com>
2020-10-26 12:40:05 -0300

    Se emplea dotenv - ver env_example para configuracion development

--- site/database/config/config.js
+++ site/database/config/config.js
@@ -1,27 +1,29 @@
+require('dotenv').config()
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "thundershoes_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "define": {
-      "timestamps": false,
-      "underscored": true
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    define: {
+      timestamps: false,
+      underscored: true
     }
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql"
   }
 }
