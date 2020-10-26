32b374e30a994ba09683baa917807c6236dcecf4
MatuteDV <matiasvieira0@gmail.com>
2020-10-26 11:16:29 -0300

    Se elimina sentencia create DB de archivo de datos

--- site/data/db/data-inserts/thundershoes_db_127_0_0_1__26_10_2020.sql
+++ site/data/db/data-inserts/thundershoes_db_127_0_0_1__26_10_2020.sql
@@ -17,13 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `thundershoes_db`
---
-CREATE DATABASE IF NOT EXISTS `thundershoes_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
-USE `thundershoes_db`;
-
---
 -- Volcado de datos para la tabla `brands`
 --
 
